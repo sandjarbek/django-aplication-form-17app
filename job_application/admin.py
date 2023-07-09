e33a342bd346c95345c148a2e32999b02452f5ec
User <sanjarbek.89.ss@gmail.com>
2023-07-09 13:48:33 +0500

    Creating admin and navbar

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("-first_name", )
+
+    readonly_fields = ("occupation",)
+
+
+
+admin.site.register(Form, FormAdmin)
